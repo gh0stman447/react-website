f45cc082fc2044d1d7fb0900629ffca0741faea6
gh0stman447 <o_malygin@bk.ru>
2024-01-04 21:37:51 +0300

    init project

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+
+
+  
+  return <div>Navbar</div>;
+};
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      neutralSilver: '#F5F7FA',
+      NeutralDGrey: '#4D4D4D',
+      brandPrimary: '#4CAF4F',
+      neutralGrey: '#717171',
+    },
+  },
+  plugins: [],  
+};
